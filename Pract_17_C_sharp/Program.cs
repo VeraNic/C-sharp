b1ec13d9f8df8a31c34138b02dabc72e1df9a18e
Vera.Frolova <v.e.frolova71@gmail.com>
2022-02-27 22:42:18 +0300

    Add tasks 17-18

---
+++ Pract_17_C_sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("");
+Console.WriteLine("___________________________");
+Console.WriteLine("По двум заданным числам проверять является ли одно квадратом другого");
+Console.WriteLine("___________________________");
+Console.WriteLine("");
+Console.Write("Введите первое число: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 == n2 * n2) Console.Write("Первое число является квадратом второго. ");
+if (n2 == n1 * n1) Console.Write("Второе число является квадратом первого. ");
+if (n1 != n2 * n2 && n2 != n1 * n1) Console.Write("Ни одно из чисел не является квадратом другого.");
---
+++ Pract_18_C_sharp/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("18. Проверить истинность утверждения ¬(X V Y) = ¬X & ¬Y");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+Console.WriteLine("X Y |F");
+Console.WriteLine("----+--");
+bool X = true;
+bool Y = true;
+bool F1 = (!(X || Y) == (!X && !Y));
+byte n = Convert.ToByte(F1);
+Console.WriteLine("1 1 |" + n); 
+
+X = true;
+Y = false;
+bool F2 = (!(X || Y) == (!X && !Y));
+n = Convert.ToByte(F2);
+Console.WriteLine("1 0 |" + n); 
+
+X = false;
+Y = true;
+bool F3 = (!(X || Y) == (!X && !Y));
+n = Convert.ToByte(F3);
+Console.WriteLine("0 1 |" + n); 
+
+X = false;
+Y = false;
+bool F4 = (!(X || Y) == (!X && !Y));
+n = Convert.ToByte(F4);
+Console.WriteLine("0 0 |" + n); 
+
+Console.WriteLine("");
+if (F1 && F2 && F3 && F4) Console.Write("Это утверждение истинно для всех значений истинности X и Y.");
+else Console.WriteLine("Это утверждение ложно.");
+Console.WriteLine("");
+Console.WriteLine("");
+// // Вариант решения задачи для вводимых пользователем значений истинности X и Y.
+// Console.Write("Введите значение истинности высказывания Х, 0 или 1: ");
+//  int i = -1;
+//        bool result = false;
+//         while (result == false)
+//         {
+//             Console.WriteLine("Введите, пожалуйста, 0 или 1! ");
+//             string s = Console.ReadLine();
+//                         result = int.TryParse(s, out i);
+//             if (s.Equals("0") || s.Equals("1") ) result = true;
+//             else result = false;
+//         }
+// Console.WriteLine(i);
+// bool X = Convert.ToBoolean(i);
+// 
+// Console.Write("Введите значение истинности высказывания Y, 0 или 1: ");
+// i = -1;
+//         result = false;
+//         while (result == false)
+//         {
+//             Console.WriteLine("Введите, пожалуйста, 0 или 1! ");
+//             string s = Console.ReadLine();
+//                         result = int.TryParse(s, out i);
+//             if (s.Equals("0") || s.Equals("1") ) result = true;
+//             else result = false;
+//         }
+// Console.WriteLine(i);
+// bool Y = Convert.ToBoolean(i);
+
+// if (!(X || Y) == (!X && !Y)) Console.Write("Это утверждение истинно.");
+// else Console.Write("Это утверждение ложно.");
+
+ 

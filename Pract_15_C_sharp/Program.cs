fb3ac9842d2d1941d9db49cd80c0c1ea4ec906fb
Vera.Frolova <v.e.frolova71@gmail.com>
2022-02-27 20:43:17 +0300

    Add task 16

--- Pract_15_C_sharp/Program.cs
+++ Pract_15_C_sharp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("");
+Console.WriteLine("___________________________");
+Console.WriteLine("15. Дано число. Проверить кратно ли оно 7 и 23");
+Console.WriteLine("___________________________");
+Console.WriteLine("");
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.Write("Данное число кратно 7 и 23");
+}
+else Console.Write("Данное число не кратно 7 и 23");
+Console.WriteLine("");
---
+++ Pract_16_C_sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("");
+Console.WriteLine("___________________________");
+Console.WriteLine(
+    "Дано число обозначающее день недели. Выяснить является номер дня недели выходным"
+);
+Console.WriteLine("___________________________");
+Console.WriteLine("");
+Console.Write("Введите номер дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+    Console.Write("Этот номер дня недели является выходным.");
+else
+    Console.Write("Этот номер дня недели не является выходным.");

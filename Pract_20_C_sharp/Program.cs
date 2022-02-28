b32f38928998221827bd8be2ff947f2e36fa3b4b
Vera.Frolova <v.e.frolova71@gmail.com>
2022-02-28 07:09:19 +0300

    Add #20

---
+++ Pract_20_C_sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("20. Задать номер четверти, показать диапазоны для возможных координат");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+Console.Write("Введите номер четверти (1, 2, 3, 4): ");
+byte n = Convert.ToByte(Console.ReadLine());
+
+switch (n)
+{
+    case 1:
+        Console.Write("x > 0, y > 0");
+        break;
+    case 2:
+        Console.Write("x < 0, y > 0");
+        break;
+    case 3:
+        Console.Write("x < 0, y < 0");
+        break;
+    case 4:
+        Console.Write("x > 0, y < 0");
+        break;
+    default:
+        Console.Write("Координатной четверти с таким номером не существует!");
+        break;
+}
---
+++ Pract_21_C_sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("21. Программа проверяет, является ли пятизначное число палиндромом.");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+
+int n = 0;
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
+if ((n / 10000 == n % 10) && ((n / 1000 - (n / 10000) * 10) == ((n % 100) - n % 10) / 10))
+    Console.WriteLine("Это палиндром.");
+else
+    Console.WriteLine("Это не палиндром. ");
+Console.WriteLine("Крайние цифры: " + n / 10000 + " " + n % 10);
+Console.WriteLine(
+    ("Вторые по краям цифры: " + (n / 1000 - (n / 10000) * 10) + " " + ((n % 100) - n % 10) / 10)
+);
+Console.WriteLine("");
+Console.WriteLine("");

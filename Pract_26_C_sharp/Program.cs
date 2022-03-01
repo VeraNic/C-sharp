5688ed283ffef1c2931f32ca191af2a3de6aa186
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-01 23:04:58 +0300

    Add #26

---
+++ Pract_26_C_sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("26. Возведите число А в натуральную степень B, используя цикл");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+Console.Write("Введите число А = ");
+int A = int.Parse(Console.ReadLine()); 
+// Console.WriteLine("");
+Console.Write("Введите натуральное число В = ");
+int B = int.Parse(Console.ReadLine()); 
+Console.WriteLine("");
+
+int counter = 1;
+int pow = 1;
+while (counter <= B)
+    { 
+    pow = pow * A;
+    counter++;
+    }
+Console.Write("Число А в степени B " + " равно " + pow);

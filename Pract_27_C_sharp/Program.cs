c1141248b3d56a953556b4b10de7cb7e3a9051d8
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-01 23:31:37 +0300

    Add #26

---
+++ Pract_27_C_sharp/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("27. Определить количество цифр в числе");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("");
+int count = 0;
+int i = num;
+while (i > 0)
+    { 
+    i =i/10;
+    count++;
+    }
+Console.Write("Количество цифр в числе " + num + " равно " + count);
+

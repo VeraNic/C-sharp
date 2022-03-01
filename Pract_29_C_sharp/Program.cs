b12ab598c7d981aba6889388d0d9d5db327c4534
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-02 02:07:59 +0300

    Add #29

---
+++ Pract_29_C_sharp/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("29. Написать программу вычисления произведения чисел от 1 до N");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+Console.Write("Введите число: N = ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("");
+
+int number = 1;
+int factorial = 1;
+while (number <= N)
+{
+    factorial = factorial * number;
+    number++;
+}
+Console.Write("Произведение чисел от 1 до N равно " + factorial);

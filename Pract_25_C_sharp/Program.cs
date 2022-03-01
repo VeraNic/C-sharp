b91d0647ca34deea23122674c96f57b48da09969
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-01 22:51:08 +0300

    Add #25

---
+++ Pract_25_C_sharp/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("25. Найти сумму чисел от 1 до А");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+Console.Write("Введите натуральное число А = ");
+int A = int.Parse(Console.ReadLine());
+
+int counter = 1;
+int sum = 0;
+while (counter <= A)
+    { 
+    sum = sum + counter;
+    counter++;
+    }
+Console.WriteLine("");
+Console.Write("Сумма чисел от 1 до " + (counter - 1) + " равна " + sum);

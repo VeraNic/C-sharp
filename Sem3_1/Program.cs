58bf5cbc4c2c853b386fc89dc6e72db5337347ec
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-04 02:38:21 +0300

    Add Sem2Dop3

---
+++ Sem3_1/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("По номеру четверти плоскости, определить диапазон координат Х и У, причем X <> 0 и Y <> 0");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+
+Console.Write("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Random rand = new Random ();
+Console.WriteLine("");
+if (n == 1) 
+{
+    Console.WriteLine("Диапазон: x > 0, y > 0. Примеры координат точек:");
+    for (int i = 0; i < 10; i++)
+    {
+    Console.WriteLine("x = " + rand.Next(1, 100) + ", y = " + rand.Next(1, 100));
+    }
+}
+if (n == 2) 
+{
+    Console.WriteLine("Диапазон: x < 0, y > 0. Примеры координат точек:");
+    for (int i = 0; i < 10; i++)
+    {
+    Console.WriteLine("x = " + -1 * rand.Next(1, 100) + ", y = " + rand.Next(1, 100));
+    }
+}
+if (n == 3) 
+{
+    Console.WriteLine("Диапазон: x < 0, y < 0. Примеры координат точек:");
+    for (int i = 0; i < 10; i++)
+    {
+    Console.WriteLine("x = " + -1 * rand.Next(1, 100) + ", y = " + -1 * rand.Next(1, 100));
+    }
+}
+if (n == 4) 
+{
+    Console.WriteLine("Диапазон: x > 0, y < 0. Примеры координат точек:");
+    for (int i = 0; i < 10; i++)
+    {
+    Console.WriteLine("x = " + rand.Next(1, 100) + ", y = " + -1 * rand.Next(1, 100));
+    }
+}

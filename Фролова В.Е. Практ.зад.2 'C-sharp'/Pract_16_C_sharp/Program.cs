90239a20cd8b8c10d718c3854f665ad01db4e852
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-03 12:40:47 +0300

    Аixed bugs

---
+++ Фролова В.Е. Практ.зад.2 'C-sharp'/Pract_16_C_sharp/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("");
+Console.WriteLine("___________________________");
+Console.WriteLine(
+    "Дано число, обозначающее день недели. Выяснить, является он выходным."
+);
+Console.WriteLine("___________________________");
+Console.WriteLine("");
+Console.Write("Введите номер дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+    Console.Write("Этот день недели является выходным.");
+else
+    Console.Write("Этот день недели не является выходным.");
---
+++ Фролова В.Е. Практ.зад.2 'C-sharp'/Pract_5_C_sharp/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе выводит перевёрнутое число.");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("");
+
+Console.Write("Введите натуральное трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+while (number < 100 || number > 999)
+{
+    Console.Write("Введённое значение не соответствует условию! Повторите ввод: ");
+number = int.Parse(Console.ReadLine());
+}
+int digital1 = number % 10;
+int digital2 = number / 10 % 10;
+int digital3 = number / 100;
+
+int result = digital1 * 100 + digital2 * 10 + digital3;
+
+Console.Write("Перевёрнутое число: " + result);

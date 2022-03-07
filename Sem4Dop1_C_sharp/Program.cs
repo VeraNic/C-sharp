229c260bb96af9f6d4e60d9cfd195bb004dda022
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-07 08:59:03 +0300

    Add Sem3_Dop1

--- Sem4Dop1_C_sharp/Program.cs
+++ Sem4Dop1_C_sharp/Program.cs
@@ -6,8 +6,7 @@ static void Main(string[] args)
         Console.WriteLine(
             "Знакомство с языками программирования. Семинар 3. Дополнительные задачи."
         );
-        Console.WriteLine("Задача 1. На ввод подаётся номер четверти.");
-        Console.WriteLine("Создаются 3 случайные точки в этой четверти. ");
+        Console.WriteLine("Задача 1. На ввод подаётся номер четверти. Создаются 3 случайные точки в этой четверти. ");
         Console.WriteLine(
             "Определите самый оптимальный маршрут для торгового менеджера, который выезжает из центра координат."
         );
@@ -41,7 +40,7 @@ static void Main(string[] args)
                 cy = 0;
                 break;
         }
-        int d = 10;
+        int d = 100;
         Random random = new Random();
         int x1 = cx * random.Next(d);
         int y1 = cy * random.Next(d);
@@ -50,7 +49,7 @@ static void Main(string[] args)
         int x3 = cx * random.Next(d);
         int y3 = cy * random.Next(d);
 
-        Console.Write("Созданы три случайные точки в выбранной вами четверти: ");
+        Console.Write("Созданы три случайные точки в указанной четверти: ");
         Console.Write("А1 (" + x1 + "; " + y1 + "), ");
         Console.Write("А2 (" + x2 + "; " + y2 + "), ");
         Console.WriteLine("А3 (" + x3 + "; " + y3 + ").");
@@ -64,17 +63,17 @@ static void Main(string[] args)
         double A1A3 = rast(x1, y1, x3, y3);
 
         double OA1A2A3 = OA1 + A1A2 + A2A3;
-        Console.Write("Варианты маршрутов: OA1A2A3 = " + Math.Round(OA1A2A3, 2));
+        Console.Write("Варианты маршрутов: OA1A2A3 = " + Math.Round(OA1A2A3, 3));
         double OA1A3A2 = OA1 + A1A3 + A2A3;
-        Console.Write(". OA1A3A2 = " + Math.Round(OA1A3A2, 2));
+        Console.Write(". OA1A3A2 = " + Math.Round(OA1A3A2, 3));
         double OA2A1A3 = OA2 + A1A2 + A1A3;
-        Console.Write(". OA2A1A3 = " + Math.Round(OA2A1A3, 2));
+        Console.Write(". OA2A1A3 = " + Math.Round(OA2A1A3, 3));
         double OA2A3A1 = OA2 + A2A3 + A1A3;
-        Console.Write(". OA2A3A1 = " + Math.Round(OA2A3A1, 2));
+        Console.Write(". OA2A3A1 = " + Math.Round(OA2A3A1, 3));
         double OA3A1A2 = OA3 + A1A3 + A1A2;
-        Console.Write(". OA3A1A2 = " + Math.Round(OA3A1A2, 2));
+        Console.Write(". OA3A1A2 = " + Math.Round(OA3A1A2, 3));
         double OA3A2A1 = OA3 + A2A3 + A1A2;
-        Console.WriteLine(". OA3A2A1 = " + Math.Round(OA3A2A1, 2) + ".");
+        Console.WriteLine(". OA3A2A1 = " + Math.Round(OA3A2A1, 3) + ".");
         Console.WriteLine("");
 
         double bestway1 = Math.Min(OA1A2A3, OA1A3A2);
@@ -89,7 +88,7 @@ static void Main(string[] args)
         if (bestway == OA2A3A1) Console.Write("Cамый оптимальный маршрут: O - A2 - A3 - A1, ");
         if (bestway == OA3A1A2) Console.Write("Cамый оптимальный маршрут: O - A3 - A1 - A2, ");
         if (bestway == OA3A2A1) Console.Write("Cамый оптимальный маршрут: O - A3 - A2 - A1, ");
-        Console.Write("его длина равна " + Math.Round(bestway, 2) + ".");
+        Console.Write("его длина равна " + Math.Round(bestway, 3) + ".");
     }
 
     public static double rast(int xA, int yA, int xB, int yB)

0728847397d73018a5bb7bc8ad4750dda8382e15
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-29 08:54:45 +0300

    Add Sem5Dop3

---
+++ Sem5Dop3_C_sharp/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("Знакомство с языками программирования. Семинар 5. Дополнительные задачи."); Console.WriteLine();
+Console.WriteLine("Задача 3. Найдите все числа от 1 до 1000000, сумма цифр которых в три раза меньше их произведений. ");
+Console.WriteLine("Подсчитайте их количество. ");
+Console.WriteLine("________________________________________________________"); Console.WriteLine();
+
+count();
+
+static void main count()
+{
+    int result = 0;
+for (int i = 1; i < 1000000; i++)
+{
+    if (sumdigits(i) * 3 == multdigits(n)) result++;
+}
+return result;
+}
+
+static void sumdigits(int n)
+{
+
+}
+
+static void multdigits(int n)
+{
+
+}
+

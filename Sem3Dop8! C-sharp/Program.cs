05a00a556ace14fdf53beffe16f0bb656b0bc2ac
Vera.Frolova <v.e.frolova71@gmail.com>
2022-03-31 00:02:28 +0300

    Add Sem6Dop1_C#

---
+++ Sem3Dop8! C-sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Задача 5. Дан массив средних температур (массив заполняется случайно) за последние 10 лет. ");
+Console.WriteLine("На ввод подают номер месяца и год начала и конца.");
+Console.WriteLine("Определить самые высокие и низкие температуры для лета, осени, зимы и весны в заданном промежутке."); 
+Console.WriteLine("Если таких температур нет, сообщить, что определить не удалось.");
+
+FillArray[] array = new int[3650];
+inputDate(int m, int year1, int year2);
+analysis(int m, int year1, int year2);
+
+FillArray()
+{
+
+}
+
+inputDate(int m, int year1, int year2)
+{
+
+}
+
+analysis(int m, int year1, int year2)
+{
+
+}
---
+++ Sem6Dop1_C#/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("________________________________________________________");
+Console.WriteLine("Знакомство с языками программирования. Семинар 6. Дополнительные задачи.");
+Console.WriteLine("Задача 1. Написать перевод десятичного числа в двоичное, используя рекурсию.");
+Console.WriteLine("________________________________________________________");
+Console.WriteLine();
+
+Console.Write("Введите десятичное число: "); 
+int numberDec = Convert.ToInt32(Console.ReadLine());
+
+string numberBin = "";
+numberBin = convertingToBin(numberDec, numberBin); Console.WriteLine();
+Console.Write("Двоичное число: " + numberBin); 
+
+static string convertingToBin(int num, string snum)
+{   
+    if (num % 2 == 0) snum = "0" + snum;
+    else snum = "1" + snum;
+   
+    if ((num/2) != 0) return convertingToBin(num/2, snum); 
+    else return snum;
+}

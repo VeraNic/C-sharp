38111ccc343489d19792281880b50653f83b57da
Vera.Frolova <v.e.frolova71@gmail.com>
2022-04-11 18:39:43 +0300

    Add Pract8_Dop1_C_sharp

---
+++ Pract8_Dop1_ C_sharp/Program.cs
@@ -0,0 +1,46 @@
+﻿int n = 4; 
+int rows = n;
+int columns = n;
+
+textTask();
+double[,] array = new double[rows, columns];
+FillArray2(array); Console.WriteLine("The array: \n"); 
+PrintArray2(array); Console.WriteLine(new string('_', 10 * columns)); Console.WriteLine("");
+ChangeArray2(array);
+Console.WriteLine("The changed array: \n"); Console.ForegroundColor = ConsoleColor.Green; 
+PrintArray2(array); Console.ResetColor();
+Console.ReadKey();
+
+void textTask()
+{
+Console.Clear(); Console.ForegroundColor = ConsoleColor.DarkYellow; Console.WriteLine(new string('_', Console.WindowWidth)); 
+Console.WriteLine("Знакомство с языками программирования. Семинар 8. Дополнтельные задачи.\n" + 
+"\nЗадача 54: 1. Дан двумерный массив. Заменить в нём элементы первой строки элементами главной диагонали. А элементы последней строки, элементами побочной диагонали.");
+Console.WriteLine(new string('_', Console.WindowWidth) + "\n"); Console.ResetColor();
+}
+
+void FillArray2(double[,] matrix2)
+{   Random rnd = new Random();
+    for (int i = 0; i < matrix2.GetLength(0); i++)
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+            matrix2[i, j] = Math.Round((rnd.NextDouble() * 200 - 100), 2); 
+}
+
+void PrintArray2(double[,] matrix2)
+{for (int i = 0; i < matrix2.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            Console.Write("{0, 10}", (matrix2[i, j]).ToString("0.00"));
+            Console.WriteLine("\n");
+        } 
+    Console.WriteLine();
+}
+
+void ChangeArray2(double[,] matrix2)
+{   (matrix2[0, n -1], matrix2[n - 1, n - 1]) = (matrix2[n - 1, n - 1], matrix2[0, n -1]); // обмен значений правых концов диагоналей
+    for (int j = 1; j < n - 1; j++)
+        {
+        matrix2[0, j] = matrix2[j, j];
+        matrix2[n - 1, n - j -1] = matrix2[j, n - j -1];
+        }
+}
--- Pract8_Dop4_ C_sharp/Program.cs
+++ Pract8_Dop4_ C_sharp/Program.cs
@@ -1,31 +1,44 @@
-﻿Console.Clear(); Console.ForegroundColor = ConsoleColor.DarkYellow; Console.WriteLine(new string('_', Console.WindowWidth)); 
-Console.WriteLine("Знакомство с языками программирования. Семинар 8. Дополнительные задачи.\n" + 
-"\nЗадача 4. Заполните двумерный массив 3х3 числами от 1 до 9 змейкой.");
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine(new string('_', Console.WindowWidth));
+Console.WriteLine(
+    "Знакомство с языками программирования. Семинар 8. Дополнительные задачи.\n"
+        + "\nЗадача 4. Заполните двумерный массив 3х3 числами от 1 до 9 змейкой."
+);
 // 1  6  7
 // 2  5  8
 // 3  4  9
 
-Console.WriteLine(new string('_', Console.WindowWidth) + "\n"); Console.ResetColor();
+Console.WriteLine(new string('_', Console.WindowWidth) + "\n");
+Console.ResetColor();
 
 int n = 3; // Количество строк, столбцов массива n x n
 int number = 1; // первый элемент массива
 
+
 //Begin
 Print2(SnakeArray2(n, number));
-Console.WriteLine("\n"); Console.ReadKey();
+Console.WriteLine("\n");
+Console.ReadKey();
 //End.
+
 
 int[,] SnakeArray2(int n, int number)
 {
-int[,]array = new int[n,n];
-int row = 0; int column = 0; // строка и столбец текущего элемента массива
-while (column < n)
-{
-    if (column % 2 == 0) for (row = 0; row < n; row++) array[row, column] = number++; // чётная вертикаль, сверху вниз
-    else for (row = n - 1; row >= 0; row--) array[row, column] = number++; // нечётная вертикаль, снизу вверх 
-column++;
-}
-return array;
+    int[,] array = new int[n, n];
+    int row = 0;
+    int column = 0; // строка и столбец текущего элемента массива
+    while (column < n)
+    {
+        if (column % 2 == 0)
+            for (row = 0; row < n; row++)
+                array[row, column] = number++; // чётный столбец, сверху вниз
+        else
+            for (row = n - 1; row >= 0; row--)
+                array[row, column] = number++; // нечётный столбец, снизу вверх
+        column++;
+    }
+    return array;
 }
 
 void Print2(int[,] array)
@@ -33,9 +46,7 @@ void Print2(int[,] array)
     for (int row = 0; row < array.GetLength(0); row++)
     {
         for (int column = 0; column < array.GetLength(1); column++)
-        {
             Console.Write(array[row, column] + " ");
-        }
         Console.WriteLine();
     }
 }

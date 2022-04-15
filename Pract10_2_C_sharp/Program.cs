313c00a7cc52f0e3b37a67da39274fcf49ecb2e9
Vera.Frolova <v.e.frolova71@gmail.com>
2022-04-15 07:35:20 +0300

    Add Pract10_3_C_sharp

--- Pract10_2_C_sharp/Program.cs
+++ Pract10_2_C_sharp/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿int m = 5;
+
+textTask();
+Console.Write("m = " + m + " >> ");
+Sequence(m);
+Console.WriteLine("\n"); 
+Console.ReadKey();
+ 
+void Sequence(int n, int i = 1, int count = 0)
+{
+    if (n == 0)
+        return;
+    n--;
+    count++;
+    if (count > i)
+    {
+        i++;
+        count = 1;
+    }
+    Console.Write(i);
+    if (i < n + i)
+        Console.Write(", ");
+    Sequence(n, i, count);
+}
+
+void textTask()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(new string('_', Console.WindowWidth));
+    Console.WriteLine(
+        "Знакомство с языками программирования. Урок 10. Практическое задание\n"
+            + "\n1. Дана монотонная последовательность, в которой каждое натуральное число n встречается ровно n раз: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, ... \n" + "Дано число m. Выведите первые m членов этой последовательности.\n"
+            + "m = 5 >> 1, 2, 2, 3, 3"
+    );
+    Console.WriteLine(new string('_', Console.WindowWidth) + "\n");
+    Console.ResetColor();
+}
---
+++ Pract10_3_C_sharp/Program.cs
@@ -0,0 +1,42 @@
+﻿int n = 10;
+
+textTask();
+Console.Write(n + " >> ");
+AllDivisors(n);
+Console.WriteLine("\n");
+Console.ReadKey();
+
+void AllDivisors(int i, int divisor = 1)
+{
+    if (i == 1)
+        return;
+
+    divisor++;
+    if (i % divisor == 0)
+    {
+        Console.Write(divisor);
+        if (divisor < i)
+            Console.Write(", ");
+        else
+            Console.Write(". ");
+
+        i /= divisor;
+        divisor = 1;
+    }
+    AllDivisors(i, divisor);
+}
+
+void textTask()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(new string('_', Console.WindowWidth));
+    Console.WriteLine(
+        "Знакомство с языками программирования. Урок 10. Практическое задание\n"
+            + "\n3. Дано натуральное число n > 1. Вывести все простые множители данного числа. \n"
+            + "10 >> 5, 2.\n"
+            + "12 >> 2, 2, 3."
+    );
+    Console.WriteLine(new string('_', Console.WindowWidth) + "\n");
+    Console.ResetColor();
+}

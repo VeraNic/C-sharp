aa8a61ff7eeecb81784bfe74c8dee820d59673cb
Vera.Frolova <v.e.frolova71@gmail.com>
2022-04-16 16:12:22 +0300

    Add Pract11_3_C_sharp

---
+++ Pract11_3_C_sharp/Program.cs
@@ -0,0 +1,41 @@
+﻿multipleVerification();
+
+void multipleVerification()
+{
+    textTask();
+    Console.Write("Введите номер года: ");
+    int year = Int32.Parse(Console.ReadLine());
+    CheckYear(year);
+    Console.ReadKey(); 
+    multipleVerification();
+}
+
+void CheckYear(int y, string result = "не является високосным")
+{
+    if (y % 4 != 0) 
+        {
+            Console.Write($"\n- {result}\n\n");
+            return;
+        }
+    if (y >= 400 && y % 100 == 0) 
+    CheckYear(y/100);                             //РЕКУРСИЯ 
+    else
+        if (y % 4 == 0)
+            {
+                result = "является високосным";
+                Console.Write($"\n- {result}\n\n");
+            }
+}
+
+void textTask()
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(new string('_', Console.WindowWidth));
+    Console.WriteLine(
+        "Знакомство с языками программирования. Урок 11. C#: от простого к практике. Практическое задание\n"
+            + "\nЗадача 3. Напишите рекурсивный метод, который принимает номер года и определяет, является ли он високосным или нет. "
+    ); 
+    Console.WriteLine(new string('_', Console.WindowWidth) + "\n");
+    Console.ResetColor();
+}
